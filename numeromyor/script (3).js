e329dbc5a2e9c1eda7eaa9c8ba99c122bdec14f3
gabrielvqz1 <126103730+gabrielvqz1@users.noreply.github.com>
2023-04-26 19:12:22 -0600

    Add files via upload

---
+++ numeromyor/script (3).js
@@ -0,0 +1,15 @@
+function mayorNum(){
+    var numero01  = parseInt (document.getElementById("numero01").value);
+    var numero02  = parseInt (document.getElementById("numero02").value);
+  if ( numero01 >= numero02 ){
+    document.write("El numero mayor es:"+ numero01 +"(numero1)");
+  }
+  else{
+    document.write("El numero mayor es: "+numero02+"(numero2)");
+  }
+}
+
+
+
+
+          
